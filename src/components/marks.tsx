2c96e6f3ecca0a7f8a5a96545adcac12431e5be4
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-01-13 18:08:49 -0500

    fix: use a constant for underline decorator styling

--- src/components/marks.tsx
+++ src/components/marks.tsx
@@ -10,13 +10,15 @@ const link: PortableTextMarkComponent<DefaultLink> = ({children, value}) => (
   <a href={value?.href}>{children}</a>
 )
 
+const underlineStyle = {textDecoration: 'underline'}
+
 export const defaultMarks: Record<string, PortableTextMarkComponent | undefined> = {
   em: ({children}) => <em>{children}</em>,
   strong: ({children}) => <strong>{children}</strong>,
   code: ({children}) => <code>{children}</code>,
 
   // @todo Consider whether or not these belong in core
-  underline: ({children}) => <span style={{textDecoration: 'underline'}}>{children}</span>,
+  underline: ({children}) => <span style={underlineStyle}>{children}</span>,
   'strike-through': ({children}) => <del>{children}</del>,
   link,
 }
