270ee2bb8911369879722ddd01614328cd4d15ce
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-01-13 20:28:56 -0500

    refactor: drop todo comment about underline mark

--- src/components/marks.tsx
+++ src/components/marks.tsx
@@ -16,8 +16,6 @@ export const defaultMarks: Record<string, PortableTextMarkComponent | undefined>
   em: ({children}) => <em>{children}</em>,
   strong: ({children}) => <strong>{children}</strong>,
   code: ({children}) => <code>{children}</code>,
-
-  // @todo Consider whether or not these belong in core
   underline: ({children}) => <span style={underlineStyle}>{children}</span>,
   'strike-through': ({children}) => <del>{children}</del>,
   link,
