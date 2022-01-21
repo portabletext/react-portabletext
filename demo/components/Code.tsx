b0b4f859c5a127152031bd98e0d1c9762728ae02
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-01-20 20:43:46 -0500

    fix: prevent prism from auto-highlighting in production

--- demo/components/Code.tsx
+++ demo/components/Code.tsx
@@ -4,6 +4,13 @@ import Refractor from 'react-refractor'
 import typescript from 'refractor/lang/typescript'
 import {PortableTextComponent} from '../../src'
 
+// Prism auto-highlights, but we only want the API, so we need to set it to manual mode
+if (typeof window !== 'undefined') {
+  const prismWindow = window as any
+  prismWindow.Prism = prismWindow.Prism || {}
+  prismWindow.Prism.manual = true
+}
+
 Refractor.registerLanguage(typescript)
 
 export interface CodeBlock {
