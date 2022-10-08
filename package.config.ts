5fed0711c6c1a360a85cca78a8b78406d3c63264
Marius Lundgård <studio@mariuslundgard.com>
2022-10-08 10:28:13 +0200

    build: use `@sanity/pkg-utils`

---
+++ package.config.ts
@@ -0,0 +1,27 @@
+import {defineConfig} from '@sanity/pkg-utils'
+import {visualizer} from 'rollup-plugin-visualizer'
+import {name, version} from './package.json'
+
+export default defineConfig({
+  extract: {
+    rules: {
+      'ae-missing-release-tag': 'off',
+      'tsdoc-undefined-tag': 'off',
+    },
+  },
+
+  minify: false,
+
+  rollup: {
+    plugins: [
+      visualizer({
+        emitFile: true,
+        filename: 'stats.html',
+        gzipSize: true,
+        title: `${name}@${version} bundle analysis`,
+      }),
+    ],
+  },
+
+  tsconfig: 'tsconfig.dist.json'
+})
