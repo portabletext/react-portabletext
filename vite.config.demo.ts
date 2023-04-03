5469a938a4ec35f65fdf81efb37c6e6f5801f966
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-04-03 11:13:06 -0700

    build(demo): use vite react plugin for demo

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 import {visualizer} from 'rollup-plugin-visualizer'
 
@@ -6,6 +7,7 @@ const pkg = require('./package.json')
 
 export default defineConfig({
   plugins: [
+    react(),
     visualizer({
       filename: path.join(__dirname, 'demo', 'dist', 'stats.html'),
       gzipSize: true,
