99588e8e62241afcd8d9a444636cbc593fefa1b6
Kadi Kraman <hellokadi@gmail.com>
2023-04-20 12:04:52 +0100

    fix: set list child index correctly

--- src/react-portable-text.tsx
+++ src/react-portable-text.tsx
@@ -136,7 +136,7 @@ const getNodeRenderer = (
     const children = node.children.map((child, childIndex) =>
       renderNode({
         node: child._key ? child : {...child, _key: `li-${index}-${childIndex}`},
-        index: index,
+        index: childIndex,
         isInline: false,
         renderNode,
       })
