be43adee57521837468859f979371c138cedff82
Cody Olsen <stipsan@gmail.com>
2025-11-05 00:12:29 +0100

    fix linter

--- src/components/merge.ts
+++ src/components/merge.ts
@@ -4,7 +4,14 @@ export function mergeComponents(
   parent: PortableTextReactComponents,
   overrides: PortableTextComponents,
 ): PortableTextReactComponents {
-  const {block, list, listItem, marks, types, ...rest} = overrides
+  const {
+    block: _block,
+    list: _list,
+    listItem: _listItem,
+    marks: _marks,
+    types: _types,
+    ...rest
+  } = overrides
   // @todo figure out how to not `as ...` these
   return {
     ...parent,
